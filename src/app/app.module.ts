f5a7c411a3430c7b9958feed287a83eab9c894bf
pranalipatil209 <pranalipatil209@gmail.com>
2017-03-24 18:19:21 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+
+const appRoutes : Routes = [
+  { path : '' , component : LandingPageComponent},
+  { path : 'test' , component : QuestionnaireComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    QuestionnaireComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes,{useHash: true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
